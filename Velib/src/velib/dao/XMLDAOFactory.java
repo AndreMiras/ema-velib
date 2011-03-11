91e7ccef2fa081c077033447deeb3c3709c7c0b0
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-11 09:41:07 +0000

    fixed dao factory errors, added xml dao factory

--- Velib/src/velib/dao/XMLDAOFactory.java
+++ Velib/src/velib/dao/XMLDAOFactory.java
@@ -9,10 +9,23 @@ package velib.dao;
  *
  * @author andre
  */
-class XMLDAOFactory {
+class XMLDAOFactory extends AbstractDAOFactory
+{
 
     public XMLDAOFactory()
     {
+    }
+
+    @Override
+    public DAO getClientDAO()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public DAO getAbonnementDAO()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
 }
