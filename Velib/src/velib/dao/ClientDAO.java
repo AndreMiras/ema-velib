7f46e7ae755ad20a9ff9e039754bf42d2fb0b9fb
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-11 09:32:39 +0000

    base dao, abstract dao and client dao work

--- Velib/src/velib/dao/ClientDAO.java
+++ Velib/src/velib/dao/ClientDAO.java
@@ -15,10 +15,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import velib.model.Client;
-import com.developpez.bean.Langage;
-import com.developpez.dao.AbstractDAOFactory;
-import com.developpez.dao.DAO;
-import com.developpez.dao.FactoryType;
+import velib.dao.AbstractDAOFactory;
+import velib.dao.DAO;
+import velib.dao.FactoryType;
 
 public class ClientDAO extends DAO<Client> {
 
@@ -26,32 +25,36 @@ public class ClientDAO extends DAO<Client> {
 
 		try {
 
-			//Si le langage n'existe pas en base, on le créé
-			if(obj.getLangage().getId() == 0){
-				DAO<Langage> langageDAO = AbstractDAOFactory    .getFactory(FactoryType.DAO_FACTORY)
-                                                                              .getLangageDAO();
-				obj.setLangage(langageDAO.create(obj.getLangage()));
+			// If the user doesn't exist
+                    /*
+			if(obj.getUser().getId() == 0)
+                        {
+				DAO<User> userDAO =
+                                        AbstractDAOFactory.getFactory(FactoryType.DAO_FACTORY).getClientDAO();
+				obj.setUser(userDAO.create(obj.getUser()));
 			}
+                     * 
+                     */
 			//Vu que nous sommes sous postgres, nous allons chercher manuellement
 			//la prochaine valeur de la séquence correspondant à l'id de notre table
-            ResultSet result = this	.connect
-                                    .createStatement(
+            ResultSet result =
+                    this.connect.createStatement(
                                     		ResultSet.TYPE_SCROLL_INSENSITIVE,
                                     		ResultSet.CONCUR_UPDATABLE
                                     ).executeQuery(
                                     		"SELECT NEXTVAL('developpeur_dev_id_seq') as id"
                                     );
-			if(result.first()){
-				long id = result.getLong("id");
-                PreparedStatement prepare = this	.connect
+			if(result.first())
+                        {
+                            long id = result.getLong("id");
+                            PreparedStatement prepare = this.connect
                                                     .prepareStatement(
                                             			"INSERT INTO developpeur (dev_id, dev_nom, dev_prenom, dev_lan_k)"+
                                             			"VALUES(?, ?, ?, ?)"
                                                     );
 				prepare.setLong(1, id);
-				prepare.setString(2, obj.getNom());
-				prepare.setString(3, obj.getPrenom());
-				prepare.setLong(4, obj.getLangage().getId());
+				prepare.setString(2, obj.getLastname());
+				prepare.setString(3, obj.getFirstname());
 
 				prepare.executeUpdate();
 				obj = this.find(id);
@@ -63,9 +66,9 @@ public class ClientDAO extends DAO<Client> {
 	    return obj;
 	}
 
-	public Developpeur find(long id) {
+	public Client find(long id) {
 
-		Developpeur dev = new Developpeur();
+		Client client = new Client();
 		try {
             ResultSet result = this .connect
                                     .createStatement(
@@ -75,52 +78,59 @@ public class ClientDAO extends DAO<Client> {
                                         "SELECT * FROM developpeur WHERE dev_id = " + id
                                      );
             if(result.first())
-                    dev = new Developpeur(
-                                        	id,
-                                        	result.getString("dev_nom"),
-                                        	result.getString("dev_prenom"),
-                                        	new LangageDAO().find(result.getLong("dev_lan_k"))
-                                        );
+            {
+                client = new Client(
+                                    id,
+                                    result.getString("dev_nom"),
+                                    result.getString("dev_prenom"));
+            }
 
 		    } catch (SQLException e) {
 		            e.printStackTrace();
 		    }
-		   return dev;
+		   return client;
 
 	}
-	public Developpeur update(Developpeur obj) {
+	public Client update(Client obj)
+        {
+            try
+            {
+                /*
+                    DAO<Langage> langageDAO = AbstractDAOFactory.getFactory(FactoryType.DAO_FACTORY)
+                                                                   .getLangageDAO();
+                    //Si le langage n'existe pas en base, on le créé
+                    if(obj.getLangage().getId() == 0)
+                    {
+                            obj.setLangage(langageDAO.create(obj.getLangage()));
+                    }
+                    //On met à jours l'objet Langage
+                    langageDAO.update(obj.getLangage());
+                 * 
+                 */
 
-		try{
-			DAO<Langage> langageDAO = AbstractDAOFactory    .getFactory(FactoryType.DAO_FACTORY)
-                                                                       .getLangageDAO();
-			//Si le langage n'existe pas en base, on le créé
-			if(obj.getLangage().getId() == 0){
-				obj.setLangage(langageDAO.create(obj.getLangage()));
-			}
-			//On met à jours l'objet Langage
-			langageDAO.update(obj.getLangage());
+                    this.connect
+            .createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                ResultSet.CONCUR_UPDATABLE
+             ).executeUpdate(
+                    "UPDATE developpeur SET dev_nom = '" + obj.getLastname() + "',"+
+                    " dev_prenom = '" + obj.getFirstname() + "',"+
+                    " WHERE dev_id = " + obj.getId()
+             );
 
-			this.connect
-                .createStatement(
-                	ResultSet.TYPE_SCROLL_INSENSITIVE,
-                    ResultSet.CONCUR_UPDATABLE
-                 ).executeUpdate(
-                	"UPDATE developpeur SET dev_nom = '" + obj.getNom() + "',"+
-                	" dev_prenom = '" + obj.getPrenom() + "',"+
-                	" dev_lan_k = '" + obj.getLangage().getId() + "'"+
-                	" WHERE dev_id = " + obj.getId()
-                 );
+                obj = this.find(obj.getId());
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
 
-			obj = this.find(obj.getId());
-	    } catch (SQLException e) {
-	            e.printStackTrace();
-	    }
-
-		return obj;
+            return obj;
 	}
 
 
-	public void delete(Developpeur obj) {
+	public void delete(Client obj)
+        {
 		try {
 
 			this.connect
