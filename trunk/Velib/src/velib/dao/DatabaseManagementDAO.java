2474263abccc902f570868d8f7aec361659d6342
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-14 19:02:49 +0000

    [db management] draft create user table db commands

--- trunk/Velib/src/velib/dao/DatabaseManagementDAO.java
+++ trunk/Velib/src/velib/dao/DatabaseManagementDAO.java
@@ -5,12 +5,18 @@
 
 package velib.dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author andre
  */
 public class DatabaseManagementDAO implements IDatabaseManagementDAO
 {
+
+    public Connection connect = ConnectionHSQL2.getInstance();
 
     public void createDatabase()
     {
@@ -24,12 +30,57 @@ public class DatabaseManagementDAO implements IDatabaseManagementDAO
 
     public void createTables()
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        createUserTable();
     }
 
     public void dropTables()
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        dropUserTable();
+    }
+
+
+    // TODO: add keys and constraints
+    private void createUserTable()
+    {
+        String tableName = "user";
+
+        System.out.println("Creating table user");
+        try
+        {
+            Statement statement = connect.createStatement();
+
+            String sql =
+                    String.format("CREATE TABLE %s" +
+                    "(id INTEGER" +
+                    "password VARCHAR", tableName);
+
+            System.out.println("SQL: " + sql);
+
+            statement.executeUpdate(sql);
+        }
+        catch (SQLException e) // TODO: handle this exception properly
+        {
+            System.out.println("Error creating user table: " + e);
+        }
+    }
+
+    private void dropUserTable()
+    {
+        String tableName = "user";
+
+        System.out.println("Dropping table user");
+        try
+        {
+            Statement statement = connect.createStatement();
+
+            String sql = "DROP TABLE " + tableName;
+
+            statement.executeUpdate(sql);
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Error dropping user table: " + e);
+        }
     }
 
 }
