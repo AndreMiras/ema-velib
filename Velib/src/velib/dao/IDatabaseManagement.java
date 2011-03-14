3c0f5af1f902f46bf41d2801d3501c068d02e32b
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-14 17:03:09 +0000

    draft database CRUD helper

---
+++ Velib/src/velib/dao/IDatabaseManagement.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package velib.dao;
+
+/**
+ *
+ * @author andre
+ *  Helper for creading/deleting database and tables
+ */
+public interface IDatabaseManagement
+{
+
+    public void createDatabase();
+
+    public void dropDatabase();
+
+    public void createTables();
+    
+    public void dropTables();
+
+}
