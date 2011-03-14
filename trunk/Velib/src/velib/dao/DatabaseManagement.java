18df3633ec70a54fa3156905dc8e78f5ee5abe3d
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-14 17:03:09 +0000

    draft database CRUD helper

---
+++ trunk/Velib/src/velib/dao/DatabaseManagement.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package velib.dao;
+
+/**
+ *
+ * @author andre
+ */
+public class DatabaseManagement implements IDatabaseManagement
+{
+
+    public void createDatabase()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void dropDatabase()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void createTables()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void dropTables()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
