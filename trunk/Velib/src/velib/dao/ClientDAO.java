21513f7f038a9ccdaf2fe9cb345b6423e2735115
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-15 07:20:01 +0000

    fixes code indentation/readability

--- trunk/Velib/src/velib/dao/ClientDAO.java
+++ trunk/Velib/src/velib/dao/ClientDAO.java
@@ -22,50 +22,48 @@ import velib.dao.FactoryType;
 public class ClientDAO extends DAO<Client>
 {
 
-	public Client create(Client obj) {
-
-		try {
-
-			// If the user doesn't exist
-                    /*
-			if(obj.getUser().getId() == 0)
-                        {
-				DAO<User> userDAO =
-                                        AbstractDAOFactory.getFactory(FactoryType.DAO_FACTORY).getClientDAO();
-				obj.setUser(userDAO.create(obj.getUser()));
-			}
-                     * 
-                     */
-			//Vu que nous sommes sous postgres, nous allons chercher manuellement
-			//la prochaine valeur de la séquence correspondant à l'id de notre table
+    public Client create(Client obj)
+    {
+        try
+        {
+            // If the user doesn't exist
+            /*
+            if(obj.getUser().getId() == 0)
+            {
+                    DAO<User> userDAO =
+                            AbstractDAOFactory.getFactory(FactoryType.DAO_FACTORY).getClientDAO();
+                    obj.setUser(userDAO.create(obj.getUser()));
+            }
+            *
+            */
             ResultSet result =
                     this.connect.createStatement(
-                                    		ResultSet.TYPE_SCROLL_INSENSITIVE,
-                                    		ResultSet.CONCUR_UPDATABLE
-                                    ).executeQuery(
-                                    		"SELECT NEXTVAL('user_id_seq') as id"
-                                    );
-			if(result.first())
-                        {
-                            long id = result.getLong("id");
-                            PreparedStatement prepare = this.connect
-                                                    .prepareStatement(
-                                            			"INSERT INTO user (id, firstname, lastname)"+
-                                            			"VALUES(?, ?, ?)"
-                                                    );
-				prepare.setLong(1, id);
-				prepare.setString(2, obj.getLastname());
-				prepare.setString(3, obj.getFirstname());
+                        ResultSet.TYPE_SCROLL_INSENSITIVE,
+                        ResultSet.CONCUR_UPDATABLE).executeQuery(
+                            "SELECT NEXTVAL('user_id_seq') as id"
+                            );
+            if(result.first())
+            {
+                long id = result.getLong("id");
+                PreparedStatement prepare = this.connect
+                                        .prepareStatement(
+                                                    "INSERT INTO user (id, firstname, lastname)"+
+                                                    "VALUES(?, ?, ?)"
+                                        );
+                    prepare.setLong(1, id);
+                    prepare.setString(2, obj.getLastname());
+                    prepare.setString(3, obj.getFirstname());
 
-				prepare.executeUpdate();
-				obj = this.find(id);
-
-			}
-	    } catch (SQLException e) {
-	            e.printStackTrace();
-	    }
-	    return obj;
-	}
+                    prepare.executeUpdate();
+                    obj = this.find(id);
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return obj;
+    }
 
 	public Client find(long id) {
 
