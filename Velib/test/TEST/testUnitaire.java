d6bfe2fff04d84e3438f17c8c030731d74c4ea4d
gvandiemen@gmail.com <gvandiemen@gmail.com@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-15 16:40:17 +0000

    Test unitaire de test (temporaire)

--- Velib/test/TEST/testUnitaire.java
+++ Velib/test/TEST/testUnitaire.java
@@ -5,11 +5,25 @@
 
 package TEST;
 
+import velib.dao.ClientDAO;
+import velib.model.Client;
+
 /**
  *
  * @author gvandiemen
  */
 public class testUnitaire
 {
+
+    public testUnitaire() {
+        Client client = new Client(0, "firstname", "lastname");
+        System.out.println("Id should be 0: " + client.getId());
+
+        ClientDAO clientDAO = new ClientDAO();
+        client = clientDAO.create(client);
+        System.out.println("Id shouldn't be 0: " + client.getId());
+        
+        clientDAO.create(client);
+    }
     
 }
