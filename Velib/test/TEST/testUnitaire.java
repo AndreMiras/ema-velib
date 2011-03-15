56ce4a5122387d60275d30a3201920d69ae93dea
gvandiemen@gmail.com <gvandiemen@gmail.com@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-15 12:09:05 +0000

    Mise en place du package test unitaires

---
+++ Velib/test/TEST/testUnitaire.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TEST;
+
+/**
+ *
+ * @author gvandiemen
+ */
+public class testUnitaire
+{
+    
+}
