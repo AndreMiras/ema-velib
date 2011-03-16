fa017b5c8d5593d719ccde2e19e7a8a601384dff
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-16 22:38:46 +0000

    binding abonnement action buttons

--- Velib/src/velib/controller/AbonnementController.java
+++ Velib/src/velib/controller/AbonnementController.java
@@ -5,6 +5,12 @@
 
 package velib.controller;
 
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ButtonModel;
+import javax.swing.JButton;
+import velib.model.AbonnementType;
 import velib.view.AbonnementPanel;
 import velib.view.MainWindowFrame;
 
@@ -21,6 +27,49 @@ public class AbonnementController extends AbstractController
     {
         super(mainWindowFrame);
         this.abonnementPanel = abonnementPanel;
+
+        abonnementPanel.addAbonnementGroupButtonListener(
+                new AbonnementTypeGroupButtonListener());
+    }
+
+
+    /*
+     * Creates and displays the appropriated abonnement page
+     * based on clicked button
+     */
+    class AbonnementTypeGroupButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            String actionCommand;
+            AbonnementType abonnementType;
+            JButton clickedButton = (JButton)e.getSource();
+
+            actionCommand = clickedButton.getActionCommand();
+            abonnementType = AbonnementType.valueOf(actionCommand);
+            
+            switch(abonnementType)
+            {
+                case HALF_DAY:
+                    System.out.println("cmd1");
+                    break;
+                case ONE_DAY:
+                    System.out.println("foo");
+                    break;
+                case ONE_WEEK:
+                    System.out.println("bar");
+                    break;
+                case ONE_MONTH:
+                    System.out.println("bar");
+                    break;
+                case ONE_YEAR:
+                    System.out.println("bar");
+                    break;
+                default:
+                    System.out.println("Unknown");
+                    break;
+            }
+        }
     }
 
     
