d49a4f06662f5a4d0f405ec0513f8436d779e3d5
nico.tomio@gmail.com <nico.tomio@gmail.com@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-16 18:17:59 +0000

    LoginScreenController, Identification OK

--- Velib/src/velib/controller/LoginScreenController.java
+++ Velib/src/velib/controller/LoginScreenController.java
@@ -55,24 +55,24 @@ public class LoginScreenController {
         {
             // TODO: use Nicolas' User DAO and stuffs
             System.out.println("Get find() the user");
-            String userPassword = view.getIdentifyTextField();
-
+            String userIdentifiant = view.getIdentifyTextField();
+            String userPassword = view.getcodeTextField();
+            
             // FIXME: we should later use the password field to find
             // the client rather than the id;
             
             // Integer idTest = Integer.parseInt(userPassword);
-            Integer idTest = 1; // TODO: testing purpose, to be removed
-            String password = "azerty";
-            String identifiant ="toto";
-            user = userDAO.find(idTest);
-             
+            //Integer idTest = 1; // TODO: testing purpose, to be removed
+
+            user = userDAO.find(userIdentifiant, userPassword);
+                         
             /*
              * If the User exists
              * Create window and associated controller
              * TODO: move this to a dedicated function for readability
              */
-            // if(user.getId() != 0)
-            if(true)
+            if(user.getId() != 0)
+            //if(true)
             {
                 // TODO: Open the next screen
                 choixIdentificationPanel = new LoggedInWelcomePanel();
@@ -80,12 +80,18 @@ public class LoginScreenController {
                         new ChoixIdentificationController(mainWindow,
                         model, choixIdentificationPanel);
                 mainWindow.setContentPanel(choixIdentificationPanel);
+                System.out.println("Users ok");
             }
             else
             {
                 // display an error message
+
+                System.out.println("Bad users");
+                //view.loginErrorPopup();
+
                 // view.loginErrorPopup();
                 System.out.println("Can't login");
+
             }
         }
     }
