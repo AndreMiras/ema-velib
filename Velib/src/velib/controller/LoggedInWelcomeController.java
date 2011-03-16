b40ef8573916dd035fdabbfcb83b62c4c7c1faca
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-16 23:36:20 +0000

    louer velo, controller and views

--- Velib/src/velib/controller/LoggedInWelcomeController.java
+++ Velib/src/velib/controller/LoggedInWelcomeController.java
@@ -5,8 +5,11 @@
 
 package velib.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import velib.model.IModel;
 import velib.view.LoggedInWelcomePanel;
+import velib.view.LouerVeloPanel;
 import velib.view.MainWindowFrame;
 
 /**
@@ -17,15 +20,38 @@ class LoggedInWelcomeController extends AbstractController
 {
 
     private IModel model ;
-    private LoggedInWelcomePanel view;
+    private LoggedInWelcomePanel loggedInWelcomePanel;
 
     public LoggedInWelcomeController(
             MainWindowFrame mainWindowFrame, IModel model,
-            LoggedInWelcomePanel view)
+            LoggedInWelcomePanel loggedInWelcomePanel)
     {
         super(mainWindowFrame);
         this.model = model;
-        this.view = view;
+        this.loggedInWelcomePanel = loggedInWelcomePanel;
+    }
+
+    private void addListeners()
+    {
+        loggedInWelcomePanel.addLouerUnVeloButtonListener(
+                new LouerUnVeloButtonListener());
+        // loggedInWelcomePanel.addRestituerUnVeloButtonListener(
+        //         new RestituerUnVeloButtonListener());
+        // view.addRechagerButtonListener(new TODO());
+
+    }
+
+    class LouerUnVeloButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            // TODO: does it really need to be kept as a private attribute?
+            LouerVeloPanel louerVeloPanel = new LouerVeloPanel();
+            LouerVeloController louerVeloController =
+                    new LouerVeloController(mainWindowFrame, louerVeloPanel);
+            // loginScreenPanel.setVisible(true);
+            mainWindowFrame.setContentPanel(louerVeloPanel);
+        }
     }
 
 
