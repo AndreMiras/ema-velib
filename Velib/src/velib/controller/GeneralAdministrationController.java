a8b810b7dc17d2f0bdb37f68e71852243abcca02
gvandiemen@gmail.com <gvandiemen@gmail.com@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-17 19:31:34 +0000

    Conbtrolleur Admin à completer

---
+++ Velib/src/velib/controller/GeneralAdministrationController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package velib.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import velib.dao.ClientDAO;
+import velib.dao.UserDAO;
+import velib.model.Client;
+import velib.model.IModel;
+import velib.model.User;
+import velib.view.GeneralAdministration;
+import velib.view.MainWindowFrame;
+
+/**
+ *
+ * @author G
+ */
+
+/*
+public class GeneralAdministrationController()
+{
+    {
+        
+        super(mainWindowFrame);
+        this.model = model;
+        this.view = view;
+        userDAO = new UserDAO();
+        clientDAO = new ClientDAO();
+
+        addListeners();
+    }
+
+    private void addListeners()
+    {
+        view.addValiderButtonListener(new ValidaterButtonListener());
+        view.ajouterVelo.addActionListener(al);
+        view.supprimerVelo.addActionListener(al);
+        view.ajouterUtilisateur.addActionListener(al);
+        view.supprimerUtilisateur.addActionListener(al);
+        view.checkDefects.addActionListener(al);
+    }
+
+}
+*/
