5390ca480d5745ac35fde39854084d0ea2870fca
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-17 23:01:26 +0000

    implemented find users by id and fixed sequence (for empty tables)

--- Velib/src/velib/dao/UserDAO.java
+++ Velib/src/velib/dao/UserDAO.java
@@ -26,6 +26,7 @@ public class UserDAO extends DAO<User>
     {
         String usersTable = tableNames[0];
          try {
+             /*
              ResultSet result = this.connect.createStatement(
                                 ResultSet.TYPE_SCROLL_INSENSITIVE,
                       		ResultSet.CONCUR_UPDATABLE
@@ -33,6 +34,15 @@ public class UserDAO extends DAO<User>
                                        "SELECT NEXT VALUE FOR sequenceuser FROM " +
                                        usersTable +
                                        " as id"
+                                    );
+              *
+              */
+
+             ResultSet result = this.connect.createStatement(
+                                ResultSet.TYPE_SCROLL_INSENSITIVE,
+                      		ResultSet.CONCUR_UPDATABLE
+                                    ).executeQuery(
+                                       "CALL NEXT VALUE FOR sequenceuser"
                                     );
             if(result.first())
             {
@@ -123,15 +133,46 @@ public class UserDAO extends DAO<User>
     }
 
     @Override
-    public User find(long id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public User find(long id)
+    {
+        User user = new User();
+        String usersTable = tableNames[0];
+
+	try
+        {
+            ResultSet result = this.connect.createStatement(
+                                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                                    ResultSet.CONCUR_READ_ONLY
+                                     ).executeQuery(
+                                    "SELECT * FROM " +
+                                    usersTable +
+                                    " WHERE id = '" + id + "'"
+                                    );
+            if(result.first())
+            {
+                //System.out.println("I'm in the result");
+                String identifiantDB = result.getString("identifiant");
+                String passwordDB = result.getString("password");
+                //System.out.println("id =" + id);
+
+                user = new User(id, identifiantDB, passwordDB);
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+        return user;
     }
 
     @Override
     public String[] createTablesStatementStrings()
     {
-        String[] statementStrings = new String[1];
+        String[] statementStrings = new String[2];
         statementStrings[0] =
+                "CREATE SEQUENCE sequenceuser START WITH 1 INCREMENT BY 1";
+        statementStrings[1] =
                     String.format("CREATE TABLE %s" +
                     "(id INTEGER, " +
                     "identifiant VARCHAR(40), " +
