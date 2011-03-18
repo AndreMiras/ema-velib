f377b2f2ce9760f5167b7327dbb4c3a470adf130
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-18 17:21:21 +0000

    added BorneSingleton

---
+++ Velib/src/velib/dao/BorneSingleton.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package velib.dao;
+
+import velib.model.Borne;
+
+/**
+ *
+ * @author andre
+ * TODO: ? with the associated controller save the Borne id within a file?
+ */
+public class BorneSingleton
+{
+
+    private static Borne borne;
+
+    public static Borne getInstance()
+    {
+        BornesDAO borneDAO;
+        
+        if(borne == null)
+        {
+            borneDAO = new BornesDAO();
+            // TODO: hardcoded to be changed
+            borne = borneDAO.find(1);
+        }
+
+        return borne;
+    }
+
+    public static void setBorne(Borne borne)
+    {
+        BorneSingleton.borne = borne;
+    }
+
+
+}
+
