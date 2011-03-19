4062150dfcc70b752df0ea86d6b6d781fb03d72c
gvandiemen@gmail.com <gvandiemen@gmail.com@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-19 11:27:11 +0000

    correction mise en forme

--- Velib/src/velib/view/EditBornFrame.java
+++ Velib/src/velib/view/EditBornFrame.java
@@ -66,9 +66,12 @@ public class EditBornFrame extends javax.swing.JFrame {
     /**
     * @param args the command line arguments
     */
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
+    public static void main(String args[])
+    {
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
                 new EditBornFrame().setVisible(true);
             }
         });
