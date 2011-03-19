f06e94e1194f82bce81a7be8c85af769cd9d8476
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-19 10:28:24 +0000

    commiting outstanding work

---
+++ Velib/src/velib/controller/RegisterFormController.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package velib.controller;
+
+/**
+ *
+ * @author andre
+ */
+public class RegisterFormController
+{
+
+}
