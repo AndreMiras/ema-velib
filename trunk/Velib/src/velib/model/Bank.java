b71a369236d9926667395b795b097ee0ba161b99
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-20 14:28:22 +0000

    saving up bank details

--- trunk/Velib/src/velib/model/Bank.java
+++ trunk/Velib/src/velib/model/Bank.java
@@ -40,6 +40,11 @@ public class Bank
         return id;
     }
 
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
     public String getCodeVerif() {
         return codeVerif;
     }
