b2730611fe2ab30863911d766eee1ba1cd5c62f6
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-20 14:28:22 +0000

    saving up bank details

--- Velib/src/velib/controller/BankDetailsController.java
+++ Velib/src/velib/controller/BankDetailsController.java
@@ -5,8 +5,13 @@
 
 package velib.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import velib.dao.BanqueDAO;
+import velib.model.Bank;
 import velib.model.Client;
 import velib.view.BankDetailsPanel;
+import velib.view.ConfirmationAbonnementPanel;
 import velib.view.MainWindowFrame;
 
 /**
@@ -25,6 +30,44 @@ public class BankDetailsController extends AbstractController
         super(mainWindowFrame);
         this.client = client;
         this.bankDetailsPanel = bankDetailsPanel;
+
+        addListeners();
+    }
+
+    private void addListeners()
+    {
+        bankDetailsPanel.addValiderButtonListener(
+                new ValiderButtonListener());
+    }
+
+    /*
+     * TODO:
+     *  - payment process
+     *  - user/password creation
+     *  - congrat screen
+     */
+    class ValiderButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            Bank bankDetails;
+            BanqueDAO bankDAO = new BanqueDAO();
+
+            bankDetails = bankDetailsPanel.getBankDetailsFromInputs();
+            bankDetails.setId(client.getBanque().getId());
+            System.out.println("Updating bank details: " + bankDetails);
+            bankDAO.update(bankDetails);
+
+            /* Calling up the create ConfirmationAbonnement  */
+            ConfirmationAbonnementPanel confirmationAbonnementPanel =
+                    new ConfirmationAbonnementPanel();
+            ConfirmationAbonnementController confirmationAbonnementController =
+                    new ConfirmationAbonnementController(
+                        client, mainWindowFrame, confirmationAbonnementPanel);
+
+            mainWindowFrame.setContentPanel(confirmationAbonnementPanel);
+
+        }
     }
 
     
