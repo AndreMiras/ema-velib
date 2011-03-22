3de395651a055d1bfe482abe7df50776d29861a4
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-22 18:22:07 +0000

    added float latitude longitude rather than int positionX positionY in Borne and BorneDAO

--- trunk/Velib/src/velib/model/Borne.java
+++ trunk/Velib/src/velib/model/Borne.java
@@ -13,16 +13,15 @@ public class Borne {
 
     private long idBorne;
     private String nomBorne;
-    private int positionX;
-    private int positionY;
+    
+    private float latitude;
+    private float longitude;
     /* is the borne already associated with a soft */
     private boolean etat;
 
     public Borne(long id, String nomBorne) {
         this.idBorne=id;
         this.nomBorne=nomBorne;
-        this.positionX=0;
-        this.positionY=0;
 
         /* by default the borne is free */
         this.etat=true;
@@ -47,12 +46,12 @@ public class Borne {
         return nomBorne;
     }
 
-    public int getPositionX() {
-        return positionX;
+    public float getLatitude() {
+        return latitude;
     }
 
-    public int getPositionY() {
-        return positionY;
+    public float getLongitude() {
+        return longitude;
     }
 
     public boolean getEtat() {
