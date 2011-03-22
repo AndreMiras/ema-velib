d847980d00359e2c09612d90e1cf0e5f2a22ffea
gvandiemen@gmail.com <gvandiemen@gmail.com@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-22 18:08:09 +0000

    CheckDefects Frame en cours

--- Velib/src/velib/view/CheckDefectsFrame.java
+++ Velib/src/velib/view/CheckDefectsFrame.java
@@ -79,12 +79,12 @@ public class CheckDefectsFrame extends javax.swing.JFrame
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-    * @param args the command line arguments
-    */
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
+    public static void main(String args[])
+    {
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
                 new CheckDefectsFrame().setVisible(true);
             }
         });
