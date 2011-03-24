1ae6da3dcbc606ab9a992208bd22cae1231d7175
gvandiemen@gmail.com <gvandiemen@gmail.com@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-24 22:11:48 +0000

    CheckDefects => possibilité de remettre vélo en service (à terminer)

--- trunk/Velib/src/velib/controller/CheckDefectsController.java
+++ trunk/Velib/src/velib/controller/CheckDefectsController.java
@@ -5,6 +5,12 @@
 
 package velib.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import velib.dao.BornetteDAO;
+import velib.dao.VeloDAO;
+import velib.model.Bornette;
+import velib.model.Velo;
 import velib.view.CheckDefectsFrame;
 
 /**
@@ -13,10 +19,39 @@ import velib.view.CheckDefectsFrame;
  */
 public class CheckDefectsController
 {
+    Velo velo;
+    BornetteDAO bornetteDAO = new BornetteDAO();
+    VeloDAO veloDAO = new VeloDAO();
+    Bornette bornette;
     private CheckDefectsFrame checkDefectsFrame;
 
     public CheckDefectsController(CheckDefectsFrame checkDefectsFrame)
     {
         this.checkDefectsFrame = checkDefectsFrame;
     }
+
+    private void addListeners()
+    {
+        checkDefectsFrame.addActivateButtonButtonListener(
+                new ActivateButtonButtonListener());
+    }
+
+    class ActivateButtonButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            System.out.println(" TOOTO");
+            /*
+            Long idVelo = checkDefectsFrame.getIDVeloFromInput();         
+            velo = new Velo (idVelo, true);
+            velo = veloDAO.create(velo);
+
+            bornette = bornetteDAO.find(idVelo);
+            bornette.setVelo(velo);
+            bornette = bornetteDAO.update(bornette);
+            checkDefectsFrame.fillUpJList(); //Rafraichit la liste des vélos en panne.
+            */
+        }
+    }
+
 }
