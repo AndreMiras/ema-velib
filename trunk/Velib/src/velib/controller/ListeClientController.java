dd54a73bf7d6af79eb677aa114e3d8cb804cff81
nico.tomio <nico.tomio@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-24 21:29:53 +0000

    ajout de la fenetre après l'appui sur liste clients

---
+++ trunk/Velib/src/velib/controller/ListeClientController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package velib.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.text.DefaultEditorKit.InsertTabAction;
+import velib.dao.IDatabaseManagementDAO;
+import velib.view.DatabaseManagementFrame;
+
+/**
+ *
+ * @author andre
+ */
+public class ListeClientController
+{
+
+    private IDatabaseManagementDAO databaseManagement;
+    private DatabaseManagementFrame view;
+
+    public ListeClientController()
+    {
+
+        addListeners();
+    }
+
+    private void addListeners()
+    {
+      
+
+    }
+
+
+    
+
+}
