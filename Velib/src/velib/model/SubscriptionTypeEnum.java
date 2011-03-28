0f4aa297e586b45401d2a4fd4305166b5f5f4422
andre.miras <andre.miras@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-28 20:08:05 +0000

    added price to enum subscription

--- Velib/src/velib/model/SubscriptionTypeEnum.java
+++ Velib/src/velib/model/SubscriptionTypeEnum.java
@@ -17,25 +17,32 @@ public enum SubscriptionTypeEnum
 {
     // long
     HALF_DAY(1,
-    12),
+    12,
+    1),
     ONE_DAY(2,
-    24),
+    24,
+    2),
             
     // short
     ONE_WEEK(3,
-    7*24),
+    7*24,
+    10),
     ONE_MONTH(4,
-    30*24),
+    30*24,
+    20),
     ONE_YEAR(5,
-    365*24);
+    365*24,
+    30);
     
     private long id;
     private long duration; // in hours
+    private long price; // in euros
 
-    private SubscriptionTypeEnum(long id, long duration)
+    private SubscriptionTypeEnum(long id, long duration, long price)
     {
         this.id = id;
         this.duration = duration;
+        this.price = price;
     }
 
 
@@ -48,6 +55,13 @@ public enum SubscriptionTypeEnum
     {
         return duration;
     }
+
+    public long getPrice()
+    {
+        return price;
+    }
+
+    
 
     @Override public String toString()
     {
