7bc15ba53718b909ae5f1acd682ec216f6283b1d
GVanDiemen@gmail.com <GVanDiemen@gmail.com@5f92ed20-585c-03f8-f705-c27c68d68bb1>
2011-03-28 20:23:44 +0000

    NETTOYAGE CODE

--- trunk/Velib/src/velib/model/Velo.java
+++ trunk/Velib/src/velib/model/Velo.java
@@ -1,14 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package velib.model;
 
-/**
- *
- * @author fteysseire
- */
 public class Velo
 {
     private long id;
